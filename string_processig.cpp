144db80bd3398458e7669cd8a3e34ddd04df9d01
dvshapkin <dvshapkin@mail.ru>
2021-03-13 11:02:08 +0700

    Create string_processig.cpp

---
+++ string_processig.cpp
@@ -0,0 +1,21 @@
+#include <string_processing.h>
+
+std::vector<std::string> SplitIntoWords(const std::string& text) {
+    vector<string> words;
+    string word;
+    for (const char c : text) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                words.push_back(word);
+                word.clear();
+            }
+        } else {
+            word += c;
+        }
+    }
+    if (!word.empty()) {
+        words.push_back(word);
+    }
+
+    return words;
+}
