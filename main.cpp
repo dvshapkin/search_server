a94b6ea66957d806944bd58c29c211526d201df2
Dmitry Shapkin <dvshapkin@mail.ru>
2021-04-20 12:49:21 +0700

    RemoveDocument - Ok

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <execution>
 
 using namespace std;
 
@@ -98,11 +99,48 @@ void Test3() {
     }
 }
 
+void Test4() {
+    SearchServer search_server("and with"s);
+
+    int id = 0;
+    for (
+        const string& text : {
+            "funny pet and nasty rat"s,
+            "funny pet with curly hair"s,
+            "funny pet and not very nasty rat"s,
+            "pet with rat and rat and rat"s,
+            "nasty rat with curly hair"s,
+    }
+            ) {
+        search_server.AddDocument(++id, text, DocumentStatus::ACTUAL, {1, 2});
+    }
+
+    const string query = "curly and funny"s;
+
+    auto report = [&search_server, &query] {
+        cout << search_server.GetDocumentCount() << " documents total, "s
+             << search_server.FindTopDocuments(query).size() << " documents for query ["s << query << "]"s << endl;
+    };
+
+    report();
+    // однопоточная версия
+    search_server.RemoveDocument(5);
+    report();
+    // однопоточная версия
+    search_server.RemoveDocument(std::execution::seq, 1);
+    report();
+    // многопоточная версия
+    search_server.RemoveDocument(std::execution::par, 2);
+    report();
+
+}
+
 int main() {
 
     Test1();
     Test2();
     Test3();
+    Test4();
 
     return 0;
 }
