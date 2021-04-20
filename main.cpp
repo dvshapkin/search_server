b5c7b3abfb0c58709e2064665d5b0faccb8a1c35
Dmitry Shapkin <dvshapkin@mail.ru>
2021-04-20 07:40:43 +0700

    ProcessQueriesJoined - ver1

--- main.cpp
+++ main.cpp
@@ -48,7 +48,7 @@ void Test2() {
 
     int id = 0;
     for (
-        const string& text : {
+        const string &text : {
             "funny pet and nasty rat"s,
             "funny pet with curly hair"s,
             "funny pet and not very nasty rat"s,
@@ -66,9 +66,35 @@ void Test2() {
     };
     id = 0;
     for (
-        const auto& documents : ProcessQueries(search_server, queries)
+        const auto &documents : ProcessQueries(search_server, queries)
             ) {
         cout << documents.size() << " documents for query ["s << queries[id++] << "]"s << endl;
+    }
+}
+
+void Test3() {
+    SearchServer search_server("and with"s);
+
+    int id = 0;
+    for (
+        const string& text : {
+            "funny pet and nasty rat"s,
+            "funny pet with curly hair"s,
+            "funny pet and not very nasty rat"s,
+            "pet with rat and rat and rat"s,
+            "nasty rat with curly hair"s,
+    }
+            ) {
+        search_server.AddDocument(++id, text, DocumentStatus::ACTUAL, {1, 2});
+    }
+
+    const vector<string> queries = {
+            "nasty rat -not"s,
+            "not very funny nasty pet"s,
+            "curly hair"s
+    };
+    for (const Document& document : ProcessQueriesJoined(search_server, queries)) {
+        cout << "Document "s << document.id << " matched with relevance "s << document.relevance << endl;
     }
 }
 
@@ -76,6 +102,7 @@ int main() {
 
     Test1();
     Test2();
+    Test3();
 
     return 0;
 }
--- process_queries.cpp
+++ process_queries.cpp
@@ -17,7 +17,23 @@ std::vector<std::vector<Document>> ProcessQueries(
 
 
 std::vector<Document> ProcessQueriesJoined(
-        const SearchServer& search_server,
-        const std::vector<std::string>& queries) {
-    return {};
+        const SearchServer &search_server,
+        const std::vector<std::string> &queries) {
+    std::vector<Document> result;
+    if (queries.size()) {
+        std::vector<std::vector<Document>> vec_of_vec(queries.size());
+        std::transform(std::execution::par, queries.cbegin(), queries.cend(), vec_of_vec.begin(),
+                       [&search_server](const std::string &query) {
+                           return search_server.FindTopDocuments(query);
+                       });
+        if (vec_of_vec.size()) {
+            result.reserve(vec_of_vec.size() * vec_of_vec[0].size());
+            for (const auto &v: vec_of_vec) {
+                for (const Document &doc: v) {
+                    result.push_back(doc);
+                }
+            }
+        }
+    }
+    return result;
 }
